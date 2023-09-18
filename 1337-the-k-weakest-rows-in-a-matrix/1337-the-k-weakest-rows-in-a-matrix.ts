9e388c1d91b47b65d5df24e487deb956ed544c9f
Tuhacrt <a067800497@gmail.com>
2023-09-18 23:43:25 +0800

    Time: 65 ms (42.86%), Space: 44.3 MB (94.64%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.ts
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.ts
@@ -1,10 +1,6 @@
 function kWeakestRows(mat: number[][], k: number): number[] {
   const counts = Array(mat.length).fill([0, 0]);
   mat.forEach((r, i) => counts[i] = [i, r.reduce((acc, cur) => acc += cur)], 0);
-
-  counts.sort(([ai, ac], [bi, bc]) => {
-    if (ac !== bc) return ac - bc;
-    return ai - bi;
-  })
+  counts.sort(([ai, ac], [bi, bc]) => ac !== bc ? ac - bc : ai - bi);
   return counts.slice(0, k).map(([i,_]) => i);
 };
