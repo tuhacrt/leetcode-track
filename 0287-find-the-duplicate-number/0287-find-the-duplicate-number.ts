f8aa7fd1dfc0b1ae70921667541e7c9c3bd276c8
Tuhacrt <a067800497@gmail.com>
2023-09-19 11:19:56 +0800

    Time: 88 ms (36.53%), Space: 57.3 MB (24.55%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.ts
@@ -0,0 +1,10 @@
+function findDuplicate(nums: number[]): number {
+  const set = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    if(set.has(nums[i])) return nums[i];
+    set.add(nums[i]);
+  }
+
+  return 0;
+};
