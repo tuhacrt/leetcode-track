fd8e13897f0893d141a1cc60fa8bd6142694eaa7
Tuhacrt <a067800497@gmail.com>
2023-09-24 14:59:59 +0800

    Time: 65 ms (100.00%), Space: 44.4 MB (100.00%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.ts
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.ts
@@ -1,10 +1,4 @@
-const count =  (xs: Array<string>, y: string) => {
-  return xs.filter(x => x === y).length;
-}
-
 function maximumOddBinaryNumber(s: string): string {
-  const ctn1 = count(s.split(''), '1');
-  return Array(ctn1 - 1).fill('1').join('') + 
-         Array(s.length - ctn1).fill('0').join('') + 
-         '1';
+  const count = s.split('').filter(x => x === '1').length;
+  return '1'.repeat(count - 1) + '0'.repeat(s.length - count) + '1';
 };
