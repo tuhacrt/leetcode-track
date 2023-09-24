0d48d9a034113d77679df1f851dad99348afd369
Tuhacrt <a067800497@gmail.com>
2023-09-24 16:21:50 +0800

    Time: 90 ms (100.00%), Space: 48.5 MB (100.00%) - LeetHub

--- 2865-beautiful-towers-i/2865-beautiful-towers-i.ts
+++ 2865-beautiful-towers-i/2865-beautiful-towers-i.ts
@@ -1,24 +1,33 @@
+const last = <T>(xs: Array<T>) => xs.length ? xs[xs.length - 1] : -1;
+
 function maximumSumOfHeights(maxHeights: number[]): number {
   const n = maxHeights.length;
-  let ans = 0;
+  const left = new Array(n + 1).fill(0);
+  let stack = [-1];
+  let [res, cur] = [0, 0];
   
   maxHeights.forEach((x, i) => {
-    let cur = x;
-    let v = x;
-    
-    for (let j = i - 1; j >= 0; j--) {
-      v = Math.min(v, maxHeights[j]);
-      cur += v;
+    while (stack.length > 1 && x <= maxHeights[last(stack)]) {
+      let j = stack.pop();
+      cur -= maxHeights[j] * (j - last(stack));
     }
-    
-    v = x;
-    
-    for (let j = i + 1; j < n; j++) {
-      v = Math.min(v, maxHeights[j]);
-      cur += v;
-    }
-    ans = Math.max(ans, cur);
+    cur += x * (i - last(stack));
+    stack.push(i);
+    left[i] = cur;
   })
   
-  return ans;
+  stack = [n];
+  cur = 0;
+  for (let i = n - 1; i >= 0; i--) {
+    const x = maxHeights[i];
+    while (stack.length > 1 && x <= maxHeights[last(stack)]) {
+      let j = stack.pop();
+      cur -= maxHeights[j] * -(j - last(stack));
+    }
+    cur += x * -(i - last(stack));
+    stack.push(i);
+    res = Math.max(res, left[i] + cur - maxHeights[i]);
+  }
+  
+  return res;
 };
