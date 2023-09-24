bffd29e8005555309f39423ea0e7510df7142c24
Tuhacrt <a067800497@gmail.com>
2023-09-24 15:01:35 +0800

    Time: 78 ms (100.00%), Space: 45 MB (100.00%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.ts
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.ts
@@ -1,4 +1,4 @@
 function maximumOddBinaryNumber(s: string): string {
-  const count = s.split('').filter(x => x === '1').length;
+  const count = s.match(/1/g).length;
   return '1'.repeat(count - 1) + '0'.repeat(s.length - count) + '1';
 };
