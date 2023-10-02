1b856432e41c859345cbd723967eebdfeab524d4
Tuhacrt <a067800497@gmail.com>
2023-10-02 13:50:53 +0800

    Time: 30 ms (83.68%), Space: 13.7 MB (11.45%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     bool winnerOfGame(string C) {
-        int n = C.size();
-        bool ans = false;
-        int As=0, Bs=0;
-        int l=0, r=0;
+        int a=0, b=0;
         
-        while (r < n) {
-            if (C[r] != C[l]){
-                if (r-l>2) {
-                    if (C[l] == 'A')
-                        As += r-l-2;
-                    else 
-                        Bs += r-l-2;
-                }
-                l = r;
-            } else if (r==n-1) {
-                if (r-l>1) {
-                    if (C[l] == 'A')
-                        As += r-l-1;
-                    else 
-                        Bs += r-l-1;
-                }
+        for (int i=1;i<C.size()-1;i++) {
+            if(C[i-1] == C[i] && C[i] == C[i+1]) {
+                if (C[i] == 'A')
+                    a++;
+                else
+                    b++;
             }
-            r++;
         }
         
-        cout <<As<<Bs<<endl;
-        
-        return As > Bs;
+        return a > b;
     }
 };
