47d8dec923d05d07ce8916dd74a065c40d627176
Tuhacrt <a067800497@gmail.com>
2023-10-05 00:30:31 +0800

    Time: 10 ms (71.10%), Space: 20.1 MB (49.13%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int trap(vector<int>& H) {
+        int l=0, r=H.size()-1;
+        int ml=0, mr=0;
+        int ans=0;
+        
+        while (l < r) {
+            if (H[l] < H[r]) {
+                H[l] >= ml ? (ml = H[l]) : ans += (ml-H[l]);
+                l++;
+            } else {
+                H[r] >= mr ? (mr=H[r]): ans+=(mr-H[r]);
+                r--;
+            }
+        }
+        
+        return ans;
+    }
+};
