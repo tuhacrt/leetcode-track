d9c9c3a680739301ada87c45611da4d256e770b0
Tuhacrt <a067800497@gmail.com>
2023-10-16 10:17:25 +0800

    Time: 0 ms (100%), Space: 6.6 MB (63.89%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,22 +1,17 @@
 class Solution {
-    unordered_map<int, vector<int>> memo;
 public:
     vector<int> getRow(int r) {
-        if (r == 0) {
-            memo[0] = {1};
-            return {1};
-        }
-        if (r == 1) {
-            memo[1] = {1, 1};
-            return {1, 1};
+        vector<int> curr, prev = {1};
+
+        for (int i = 1; i <= r; i++) {
+            curr.assign(i + 1, 1);
+
+            for (int j = 1; j < i ; j++)
+                curr[j] = prev[j - 1] + prev[j];
+
+            prev = move(curr);
         }
 
-        vector<int> last = getRow(r - 1);
-        vector<int> ans = vector(r + 1, 1);
-        for (int i = 1; i < r; i++) {
-            ans[i] = last[i - 1] + last[i];
-        }
-        memo[r] = ans;
-        return ans;
+        return prev;
     }
 };
