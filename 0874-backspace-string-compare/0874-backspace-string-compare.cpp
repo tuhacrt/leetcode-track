f4f6b5b93451b5842910eff3529228c8808045f9
Tuhacrt <a067800497@gmail.com>
2023-10-19 21:17:33 +0800

    Time: 0 ms (100%), Space: 6.6 MB (39.46%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> st;
-        string c1, c2;
+        int i = s.size() - 1, j = t.size() - 1;
+        int skipS = 0, skipT = 0;
 
-        for (auto& c: s) {
-            if (c != '#') st.push(c);
-            else if (st.size()) st.pop();
+        while (i >= 0 || j >=0) {
+            while (i >= 0) {
+                if (s[i] == '#') {skipS++; i--;}
+                else if (skipS > 0) {skipS--; i--;}
+                else break;
+            }
+
+            while (j >= 0) {
+                if (t[j] == '#') {skipT++; j--;}
+                else if (skipT > 0) {skipT--; j--;}
+                else break;
+            }
+
+            if (i >= 0 && j >= 0 && s[i] != t[j])
+                return false;
+            if ((i >= 0) != (j >= 0))
+                return false;
+            i--; j--;
         }
-
-        while (st.size()) {
-            char c = st.top();
-            st.pop();
-            c1 += c;
-        }
-        
-        for (auto& c: t) {
-            if (c != '#') st.push(c);
-            else if (st.size()) st.pop();
-        }
-
-        while (st.size()) {
-            char c = st.top();
-            st.pop();
-            c2 += c;
-        }
-
-        if (c1.size() != c2.size()) return false;
-
-        for (int i = 0; i < c1.size(); i++) {
-            if (c1[i] != c2[i]) return false;
-        }
-
         return true;
     }
 };
