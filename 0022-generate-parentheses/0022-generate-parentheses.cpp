265fa5507deda8c42f8c7e106a2a8d32e43214cb
Tuhacrt <a067800497@gmail.com>
2023-10-19 21:54:18 +0800

    Time: 0 ms (100%), Space: 16.9 MB (10.87%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
+    vector<string> ans;
+    int n;
     vector<string> generateParenthesis(int n) {
-        vector<string> ans = {};
-        bt(ans, "", 0, 0, n);
+        this->ans = {};
+        this->n = n;
+        bt("", 0, 0);
         return ans;
     }
 
-    void bt(vector<string>& ans, string cur, int l, int r, int n) {
+    void bt(string cur, int l, int r) {
         if (cur.size() == 2 * n) {
             ans.push_back(cur);
             return;
         }
         if (l < n) {
             cur += "(";
-            bt(ans, cur, l + 1, r, n);
+            bt(cur, l + 1, r);
             cur.pop_back();
         }
         if (l > r) {
             cur += ")";
-            bt(ans, cur, l, r + 1, n);
+            bt(cur, l, r + 1);
             cur.pop_back();
         }
     }
