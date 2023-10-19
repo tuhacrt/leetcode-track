062a79bf3f3d1be0fd12caf46c29e0576f531c25
Tuhacrt <a067800497@gmail.com>
2023-10-19 23:39:21 +0800

    Time: 308 ms (5%), Space: 133.7 MB (5.3%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -5,20 +5,17 @@ public:
         stack<vector<int>> st({ 
             { t[n - 1], n - 1 } 
         });
-        vector<int> ans = { 0 };
+        vector<int> ans(n, 0);        
 
         for (int i = n - 2; i >= 0; i--) {
             while (st.size() != 0 && t[i] >= st.top()[0]) 
                 st.pop();   
-            if (st.size() == 0) 
-                ans.push_back(0);
-            else 
-                ans.push_back(st.top()[1] - i);
+            if (st.size() != 0) 
+                ans[i] = st.top()[1] - i;
             
             st.push({ t[i], i });
         }
 
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
